125bb2e6357de2764f5c8ca1b8ac8368b65e7a6d
Juan Facundo Tam <107519577+juanfacundotam@users.noreply.github.com>
2023-10-19 11:15:23 -0300

    Update Form.tsx

--- react-ts-blog/src/components/Form.tsx
+++ react-ts-blog/src/components/Form.tsx
@@ -1,31 +1,56 @@
 const Form = () => {
-    const [inputValues, setInputValues] = ({
-        nick: '',
-        subMonths: 0,
-        avatar: '',
-        description: ''
-    })
- const handleSubmit = () => {
+  const [inputValues, setInputValues] = {
+    nick: "",
+    subMonths: 0,
+    avatar: "",
+    description: "",
+  };
+  const handleSubmit = () => {};
 
- }  
-
- const handleChange = e => {
+  const handleChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setInputValues({
-        ...inputValues, [e.target.name]: e.target.value
-    })
- }
- return (
+      ...inputValues,
+      [evt.target.name]: evt.target.value,
+    });
+  };
+  return (
     <div>
-        <form action="
-        ">
-            <input value={inputValues.nick} type="text" name="nick" placeholder="nick"/>
-            <input value={inputValues.subMonths} type="number" name="subMonths" placeholder="subMonths"/>
-            <input value={inputValues.avatar} type="text" name="avatar" placeholder="avatar"/>
-            <input value={inputValues.description} type="text" name="description" placeholder="description"/>
-            <button>Save new sub!</button>
-        </form>
+      <form
+        action="
+        "
+      >
+        <input
+          onChange={handleChange}
+          value={inputValues.nick}
+          type="text"
+          name="nick"
+          placeholder="nick"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.subMonths}
+          type="number"
+          name="subMonths"
+          placeholder="subMonths"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.avatar}
+          type="text"
+          name="avatar"
+          placeholder="avatar"
+        />
+        <input
+          onChange={handleChange}
+          value={inputValues.description}
+          type="text"
+          name="description"
+          placeholder="description"
+        />
+        <button>Save new sub!</button>
+      </form>
     </div>
- ) 
-}
+  );
+};
 
 export default Form;
